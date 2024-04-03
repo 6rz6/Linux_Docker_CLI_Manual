0ef4142606cf18ef7f162c4acb80a72d7d24187e
Rudyz <102882394+6rz6@users.noreply.github.com>
2024-04-04 06:14:32 +0700

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -6,28 +6,28 @@
 # 3. Run the Script using: ./docker_install.sh
 
 # Update Package Index
-sudo apt update -y
+sudo apt update -y \
 
 # Install Required Packages
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common \
 
 # Download and Add Docker GPG Key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg \
 
 # Add Docker Repository
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
 
 # Update Package Index
-sudo apt update -y
+sudo apt update -y \
 
 # Install Docker CE
-sudo apt install -y docker-ce
+sudo apt install -y docker-ce \
 
 # Start Docker Daemon
-sudo systemctl start docker
+sudo systemctl start docker \
 
 # Enable Docker to Start on Boot
-sudo systemctl enable docker
+sudo systemctl enable docker \
 
 
      
